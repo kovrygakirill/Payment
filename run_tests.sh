6cb1301e4ffe7dd02c5a7c4de27e3cfaa174b779
Kirill <kovrygakirill@mail.ru>
2023-02-21 19:19:47 +0300

    add bash script for run tests

---
+++ run_tests.sh
@@ -0,0 +1,20 @@
+# Colors
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
+
+BASE_DIR=${1-backend}
+SETTINGS=$(2---settings=config.settings.test)
+
+echo "${green}>>> Create test DB for project... ${reset}"
+sleep 1
+cd $BASE_DIR
+
+python manage.py migrate $SETTINGS
+echo "${green}>>> Migration was made${reset}"
+sleep 1
+
+echo "${green}>>> Start all tests ... ${reset}"
+sleep 1
+python manage.py test tests $SETTINGS
+cd ..
